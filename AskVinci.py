a47229cf2f512fe4c7a3fd0472560b03fe9db5f6
NoamBechhofer <noam.bechhofer@gmail.com>
2023-01-24 17:56:07 -0500

    Basic working version. Bugs: cannot handle requests over 4096 bytes,       some characters get malformed

--- AskVinci.py
+++ AskVinci.py
@@ -1,5 +1,6 @@
 import tkinter.messagebox as messagebox
 import re
+import codecs
 import configparser
 import logging
 
@@ -22,10 +23,29 @@ VERSION = config.get("AskVinci", "VERSION")
 
 openai.api_key = config.get("AskVinci", "OPENAPI_KEY")
 
+ESCAPE_SEQUENCE_RE = re.compile(r'''
+    ( \\U........      # 8-digit hex escapes
+    | \\u....          # 4-digit hex escapes
+    | \\x..            # 2-digit hex escapes
+    | \\[0-7]{1,3}     # Octal escapes
+    | \\N\{[^}]+\}     # Unicode characters by name
+    | \\[\\'"abfnrtv]  # Single-character escapes
+    )''', re.UNICODE | re.VERBOSE)
 
-def expandasciiescapes(text: str) -> str:
+
+def decode_escapes(text: str) -> str:
     """
-    Replace all ASCII escapes in a URI with their corresponding characters.
+    Replace all escapes in a string with their corresponding characters.
+    """
+    def decode_match(match):
+        return codecs.decode(match.group(0), 'unicode-escape')
+
+    return ESCAPE_SEQUENCE_RE.sub(decode_match, text)
+
+
+def decode_formatters(text: str) -> str:
+    """
+    Replace all format specifications in a URI with their corresponding characters.
 
     This function expects a properly formatted URI. Any occurence of the
     character '%' must be followed by a valid two-digit uppercase hex number.
@@ -59,8 +79,11 @@ with socket.socket() as localsock:
         clntconn, clntaddr = localsock.accept()
         with clntconn:
             logging.info("accepted connection from {}".format(clntaddr))
-            # need to allow for requests > 4096 though!
+
             query = str(clntconn.recv(4096))
+            # while len(nextblock := clntconn.recv(4096)) > 0:
+            #     query += str(nextblock)
+
             if not query:
                 continue
 
@@ -69,10 +92,16 @@ with socket.socket() as localsock:
                 continue
             query = query[1][1::]
 
-            query = expandasciiescapes(query)
+            query = decode_formatters(query)
             if query == "favicon.ico":
-                clntconn.send(bytes("HTTP/1.0 404 Not Found", "utf-8"))
-                continue
+
+                clntconn.sendall(bytes("HTTP/1.0 200 OK\r\n"
+                                       "Content-Type: image/x-icon\r\n"
+                                       + "Server: AskVinci/{}\r\n".format(VERSION) +
+                                       "\r\n",
+                                       "utf-8"))
+                clntconn.sendfile(open("favicon.ico", "rb"))
+                clntconn.close()
 
             logging.info("received query: \"{}\"".format(query))
 
@@ -83,33 +112,34 @@ with socket.socket() as localsock:
                 max_tokens=MAX_TOKENS,
                 temperature=TEMPERATURE
             )
-            
+
             logging.debug("OpenAI response: " + str(response))
 
             textline = re.search(r"\"text[ -~]+\n", str(response))
             if not textline:
                 continue
+            completion = ""
             try:
-                textline = textline.group(0)  # TODO: slice this
+                completion = textline.group(0)  # group 0 is the entire match
+                # grab the completion
+                completion = decode_escapes(completion[9:len(completion) - 2:])
             except IndexError as e:
                 messagebox.showerror(
                     "AskVinci", "Askvinci: bad response format.\n{}".format(e))
 
-            print(textline)  # TODO: remove
-
             htmlbody = ("<html>\r\n"
                         "<body>\r\n"
-                        + "<h1> Query: \"{}\"</h1>\r\n".format(query) +
-                        "<h2>DaVinciSez:</h2>\r\n"
-                        + "<p>{}</p>\r\n".format(textline) +
+                        "<h1>DaVinciSez:</h1>\r\n"
+                        + "<i>{}</i>{}\r\n".format(query, completion.replace("\n", "<br>")) +
                         "</body>\r\n"
                         "</html>\r\n")
 
             message = ("HTTP/1.0 200 OK\r\n"
-                       "Content-Type: text/html"
-                       + "Content-Length: {}".format(len(htmlbody))
-                       + "Server: AskVinci/{}".format(VERSION) +
-                       "\r\n")
+                       "Content-Type: text/html\r\n"
+                       + "Content-Length: {}\r\n".format(len(htmlbody))
+                       + "Server: AskVinci/{}\r\n".format(VERSION) +
+                       "\r\n"
+                       + htmlbody)
 
             clntconn.sendall(bytes(message, "utf-8"))
             clntconn.close()
